cd693452c998fa615ab82455759ff69c3f42999b
zkan <zkan.cs@gmail.com>
2021-03-06 11:31:14 +0700

    Change to RestController, so we can remove ResponseBody :bear:

--- activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
+++ activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import team.odds.activityservice.models.Activity;
 
-@Controller
+@RestController
 public class ActivityController {
     @RequestMapping(value = "/activities", method = RequestMethod.GET)
-    @ResponseBody
     public Activity getActivity() {
         Activity activity = new Activity();
         activity.setId(1);
