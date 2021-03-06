06db445eddd9626ad82502e979a503cc218c6d2a
zkan <zkan.cs@gmail.com>
2021-03-06 11:30:11 +0700

    Set base packages to cover all inner packages :bear:

--- activityservice/src/main/java/team/odds/activityservice/ActivityServiceApplication.java
+++ activityservice/src/main/java/team/odds/activityservice/ActivityServiceApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"team.odds.activityservice", "team.odds.utils"})
+//@ComponentScan(basePackages = {"team.odds.activityservice", "team.odds.utils"})
+@ComponentScan(basePackages = {"team.odds"})
 public class ActivityServiceApplication {
 
 	public static void main(String[] args) {
