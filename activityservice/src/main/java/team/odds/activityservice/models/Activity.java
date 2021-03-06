b2a39aa748abe63506db83b2b115a418edb1f9f4
zkan <zkan.cs@gmail.com>
2021-03-06 11:36:24 +0700

    Use Lombok to reduce getter & setter code :bear:

--- activityservice/src/main/java/team/odds/activityservice/models/Activity.java
+++ activityservice/src/main/java/team/odds/activityservice/models/Activity.java
@@ -1,30 +1,11 @@
 package team.odds.activityservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Activity {
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     private Integer id;
     private String name;
     private String description;
