1ac79851020f4c7565fc5a268a8003b4445d5657
zkan <zkan.cs@gmail.com>
2021-03-06 11:15:56 +0700

    Create activity and return data :bear:

--- activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
+++ activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import team.odds.activityservice.models.Activity;
 
 @Controller
 public class ActivityController {
     @RequestMapping(value = "/activities", method = RequestMethod.GET)
     @ResponseBody
-    public String getActivity() {
-        return "Hey, Universe!";
+    public Activity getActivity() {
+        Activity activity = new Activity();
+        activity.setId(1);
+        activity.setName("Login");
+        activity.setDescription("Kan is logged in");
+
+        return activity;
     }
 }
