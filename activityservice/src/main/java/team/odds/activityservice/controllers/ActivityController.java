0fbec1f22f878bd31c7140873b5349c2ee9efcdd
zkan <zkan.cs@gmail.com>
2021-03-06 11:31:54 +0700

    Use GetMapping instead :bear:

--- activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
+++ activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
@@ -1,15 +1,13 @@
 package team.odds.activityservice.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.odds.activityservice.models.Activity;
 
 @RestController
 public class ActivityController {
-    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+//    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+    @GetMapping(value = "/activities")
     public Activity getActivity() {
         Activity activity = new Activity();
         activity.setId(1);
