01ac05cb9424b1ef2f34dfc6afdaea1a2d638867
zkan <zkan.cs@gmail.com>
2021-03-06 11:41:35 +0700

    Use builder to create object :bear:

--- activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
+++ activityservice/src/main/java/team/odds/activityservice/controllers/ActivityController.java
@@ -9,11 +9,10 @@ public class ActivityController {
 //    @RequestMapping(value = "/activities", method = RequestMethod.GET)
     @GetMapping(value = "/activities")
     public Activity getActivity() {
-        Activity activity = new Activity();
-        activity.setId(1);
-        activity.setName("Login");
-        activity.setDescription("Kan is logged in");
-
-        return activity;
+        return Activity.builder()
+                .id(1)
+                .name("Login")
+                .description("Kan is logged in")
+                .build();
     }
 }
--- activityservice/src/main/java/team/odds/activityservice/models/Activity.java
+++ activityservice/src/main/java/team/odds/activityservice/models/Activity.java
@@ -1,8 +1,14 @@
 package team.odds.activityservice.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Activity {
     private Integer id;
     private String name;
