f228580d3817fedce9bd1414a07d20fa701c0048
zkan <zkan.cs@gmail.com>
2021-03-06 13:32:46 +0700

    Mock activity service and make it return 2 activities :bear:

--- activityservice/src/test/java/team/odds/activityservice/controllers/ActivityControllerTest.java
+++ activityservice/src/test/java/team/odds/activityservice/controllers/ActivityControllerTest.java
@@ -2,17 +2,39 @@ package team.odds.activityservice.controllers;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import team.odds.activityservice.models.Activity;
 import team.odds.activityservice.services.ActivityService;
 
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 public class ActivityControllerTest {
+    @Mock
+    private ActivityService activityService;
+
+    @InjectMocks
+    private ActivityController activityController;
+
     @Test
     void testGetActivity() {
-        ActivityService activityService = new ActivityService();
-        ActivityController activityController = new ActivityController(activityService);
+//        ActivityService activityService = new ActivityService();
+//        ActivityController activityController = new ActivityController(activityService);
+
+        Activity activity1 = new Activity();
+        activity1.setName("Login");
+
+        Activity activity2 = new Activity();
+
+        List<Activity> activities = List.of(activity1, activity2);
+        Mockito.when(activityService.getAll()).thenReturn(activities);
+
         List<Activity> actual = activityController.getActivity();
+
         Assertions.assertEquals(2, actual.size());
         Assertions.assertEquals("Login", actual.get(0).getName());
     }
