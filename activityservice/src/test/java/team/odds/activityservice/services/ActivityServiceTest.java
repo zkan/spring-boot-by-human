52333cdf632f5689ebbef8e6dcd23408c5e73064
zkan <zkan.cs@gmail.com>
2021-03-06 14:29:22 +0700

    Fix test that calls repository :bear:

--- activityservice/src/test/java/team/odds/activityservice/services/ActivityServiceTest.java
+++ activityservice/src/test/java/team/odds/activityservice/services/ActivityServiceTest.java
@@ -2,14 +2,40 @@ package team.odds.activityservice.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.AbstractCharInputReader;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import team.odds.activityservice.models.Activity;
+import team.odds.activityservice.repositories.ActivityRepository;
 
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 class ActivityServiceTest {
+    @Mock
+    ActivityRepository activityRepository;
+
+    @InjectMocks
+    ActivityService activityService;
+
     @Test
     void testGetAll() {
-        ActivityService activityService = new ActivityService();
+        Activity activity1 = new Activity();
+        activity1.setId(1);
+        activity1.setName("Login");
+        activity1.setDescription("Kan is logged in");
+
+        Activity activity2 = new Activity();
+        activity2.setId(2);
+        activity2.setName("Logout");
+        activity2.setDescription("Kan is logged out");
+
+        List<Activity> activities = List.of(activity1, activity2);
+        Mockito.when(activityRepository.findAll()).thenReturn(activities);
+
         List<Activity> actual = activityService.getAll();
         Assertions.assertEquals(2, actual.size());
         Assertions.assertEquals(1, actual.get(0).getId());
